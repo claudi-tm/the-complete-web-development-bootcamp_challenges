3ab1f0047f84291594de389bd2e6f2709c69ab8a
Cláudio Tomás <ccaducarlos@gmail.com>
2023-02-12 21:55:01 +0200

    finished

--- drum_kit/index.js
+++ drum_kit/index.js
@@ -1,26 +1,53 @@
-let drum_buttons = document.querySelectorAll("button");
-
+let drum_buttons = getButtons();
+renderizeImages(drum_buttons);
 let drum_sounds = initializeAudiosFiles(drum_buttons.length);
-drum_buttons.forEach((button, index) => {
-	button.addEventListener("click", function () {
-		drum_sounds[index].play();
-		// console.log(drum_sounds.length);
+playSoundByClick(drum_buttons);
+playSoundByPressingKey(drum_buttons, drum_sounds);
+
+function getButtons() {
+	return document.querySelectorAll("button");
+}
+
+function playSoundByClick(drum_buttons) {
+	drum_buttons.forEach((button, index) => {
+		button.addEventListener("click", function () {
+			drum_sounds[index].play();
+		});
 	});
-	// button.addEventListener("keypress", (key) => {
-	// 	let name = key.key;
-	// 	alert(name);
-	// });
-});
+}
+
+function playSoundByPressingKey(drum_buttons, drum_sounds) {
+	document.addEventListener("keydown", (keyboard) => {
+		drum_buttons.forEach((button, index) => {
+			if (keyboard.key === button.textContent) drum_sounds[index].play();
+		});
+	});
+}
+
+function renderizeImages(drum_buttons) {
+	let images_src_path = [
+		"images/tom1.png",
+		"images/tom2.png",
+		"images/tom3.png",
+		"images/tom4.png",
+		"images/crash.png",
+		"images/kick.png",
+		"images/snare.png",
+	];
+	drum_buttons.forEach((button, index) => {
+		button.style.backgroundImage = `url('${images_src_path[index]}')`;
+	});
+}
 
 function initializeAudiosFiles(drum_buttons_lenght) {
 	let audio_src_path = [
-		"sounds/crash.mp3",
-		"sounds/kick-bass.mp3",
-		"sounds/snare.mp3",
 		"sounds/tom-1.mp3",
 		"sounds/tom-2.mp3",
 		"sounds/tom-3.mp3",
 		"sounds/tom-4.mp3",
+		"sounds/crash.mp3",
+		"sounds/kick-bass.mp3",
+		"sounds/snare.mp3",
 	];
 	let audios = new Array(drum_buttons_lenght);
 	console.log(drum_buttons_lenght);
